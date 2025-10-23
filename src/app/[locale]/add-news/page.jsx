ffc419b0cf46b3fb7dc9a1d548497d10e2cc8f5a
azizbeAnvarjanov <azizbeanvarjanov@gmail.com>
2025-10-23 09:48:30 +0500

    change i

--- src/app/[locale]/add-news/page.jsx
+++ src/app/[locale]/add-news/page.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import AddNews from "../../(customComponents)/AddNews";
 
 const AddNewsPage = () => {
-  return (
-    <div className="py-5">
-      {/* <AddNews /> */}
-    </div>
-  );
+  return <div className="py-5">a{/* <AddNews /> */}</div>;
 };
 
 export default AddNewsPage;
